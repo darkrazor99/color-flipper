9a7696577de33d9f6ae6cad64daeb346a99a7deb
darkrazor99 <tarektheman99@gmail.com>
2023-09-11 06:09:57 +0200

    finished simple

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+function genRandNum(maxLimit){
+    return Math.floor(Math.random()*maxLimit);
+}
+document.getElementById("btn").addEventListener("click", (e) => {
+    const randIndex = genRandNum(colors.length);
+    document.body.style.backgroundColor = colors[randIndex];
+    document.querySelector('.color').textContent = colors[randIndex];
+});
