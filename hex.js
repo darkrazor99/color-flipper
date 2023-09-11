40ad02e0477bfd57b2ef4800b726bbc85835c706
tarek <36449641+darkrazor99@users.noreply.github.com>
2023-09-11 12:40:09 +0300

    Update hex.js

--- hex.js
+++ hex.js
@@ -3,7 +3,6 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 document.getElementById("btn").addEventListener("click", (e) => {
     const rand = "#"+genRandHex(hex).join("");
-    console.log(rand);
     document.body.style.backgroundColor = rand;
     document.querySelector('.color').textContent = rand;
 });
